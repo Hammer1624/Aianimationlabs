deac826b8b543d5ebe47a77cb741ef6b42d82b5a
Hammer1624 <98486449+Hammer1624@users.noreply.github.com>
2023-10-04 15:08:13 -0400

    Lab 3 update

---
+++ lab 3/Lab3/Assets/RayCast.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class RayCast : MonoBehaviour
+{ 
+    [SerializeField] GameObject Target;
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetTarget();
+    }
+
+    private void SetTarget()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Target.transform.position = hit.point;
+            }
+        }
+    }
+}
